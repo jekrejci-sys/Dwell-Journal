cc0ecbc956800b7fffcefa59880647a90dcd51c7
jekrejci-sys <jekrejci@gmail.com>
2026-03-13 22:31:38 -0500

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -5,7 +5,7 @@ const supabaseUrl = 'https://cniggzjuzgywtamizumk.supabase.co'
 const supabaseKey = 'sb_publishable_MsqnK6DSPCaQ5B1oLdl71A_Q_lyidYB'
 
 // Free ESV API key from https://api.esv.org
-export const ESV_API_KEY = 'Token 3992bfa55cb535a76c737b04a8c3c8098a46eef3'
+export const ESV_API_KEY = '1f864b55276267b1bc4958e3de072c281f10fe12'
 
 export function createClient() {
   return createSupabaseClient(supabaseUrl, supabaseKey)
